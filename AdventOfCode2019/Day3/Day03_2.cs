a1acc492c705dfea5b3e329fd99369b58fd9accd
Tobias Waggoner <tobias.waggoner@gmail.com>
2021-12-01 18:34:11 +0100

    feat: added 2019/3

---
+++ AdventOfCode2019/Day3/Day03_2.cs
@@ -0,0 +1,29 @@
+﻿// *********************************************************
+// (c) 2021 - 2021 Netzalist GmbH & Co.KG
+// *********************************************************
+
+#region Using
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+#endregion
+
+namespace AdventOfCode2019.Day3
+{
+    public class Day03_2 : PuzzleBase
+    {
+        protected override string RunPuzzle()
+        {
+            var input = ReadInputDataAsStringArray("Day3");
+            var result = CalculateResult(input);
+            return $"Result: {result}";
+        }
+
+        private static int CalculateResult(List<string> input)
+        {
+            return CrossWireCalculator.FewestStepsToReachAnyCrossing(input[0], input[1]);
+        }
+    }
+}
